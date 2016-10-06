24d2f693d3008c41b94ecef01646083d2cca687c
Pablo González <pablo_donaire29@hotmail.com>
2016-10-06 19:22:32 -0400

    Update temporary_token.py

--- Django/temporary_token.py
+++ Django/temporary_token.py
@@ -14,37 +14,51 @@ class TemporaryToken:
     def __init__(self, user):
         self.user = user
         
-    def hash_user_encode(self, hours_limit=None):
+    def hash_user_encode(self, hours_limit=None, extra_token=''):
         """
             This method allow create a temporary or unlimited token 
             for users.
             hours_limit => int(24) for 1 day
+            extra_token => a string value for more security
+                This extra token not support the next caracter: "|"
         """
         uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
         token = default_token_generator.make_token(self.user)
         date = datetime.datetime.now()
         date_str = date.isoformat()
         until = date + datetime.timedelta(hours=hours_limit) if hours_limit else 'unlimited'
-        token_hash = '%s|%s|%s|%s' % (uidb64, token, date_str, until)
+        token_hash = '%s|%s|%s|%s|%s' % (
+            uidb64, token, date_str, until, extra_token)
         return base64.urlsafe_b64encode(bytes(token_hash))
         
-    def hash_user_valid(self, token):
+    def hash_user_valid(self, token, extra_token=''):
         """
             This method allow to check if token is valid either 
             temporary or unlimited.
         """
+        token_split = token.split("=")
+        # if the token is altered
+        if (token_split[-1:][0] != "" or len(token_split) > 3
+            or token_split[1] != ""): 
+            return False
         try:
             token_hash = base64.urlsafe_b64decode(bytes(token))
         except: return False
-        uidb64, token_decoded, date, limit = token_hash.split('|')
-        valid_uid = int(urlsafe_base64_decode(uidb64)) == self.user.pk
-        valid_token = token_decoded == default_token_generator.make_token(self.user)
+        uidb64, token_decoded, date, limit, token_added = \
+            token_hash.split('|')
+        valid_uid = int(urlsafe_base64_decode(uidb64)) == \
+            self.user.pk
+        valid_token = token_decoded == \
+            default_token_generator.make_token(self.user)
         present = datetime.datetime.now()
         if not self.hash_has_limit(token):
             limit = present + datetime.timedelta(minutes=1)
         else:
             limit = dateutil.parser.parse(limit)
-        if valid_uid and valid_token and (present < limit):
+
+        valid_extra_token = token_added == extra_token
+        if (valid_uid and valid_token and (present < limit)
+            and valid_extra_token):
             return True
         return False
     
@@ -57,6 +71,16 @@ class TemporaryToken:
         except: return False
         uidb64 = token_hash.split('|')[0]
         return int(urlsafe_base64_decode(uidb64))
+
+    def hash_get_extra_token(self, token):
+        """
+            Method that return the user id
+        """
+        try:
+            token_hash = base64.urlsafe_b64decode(bytes(token))
+        except: return False
+        extra_token = token_hash.split('|')[4]
+        return extra_token
     
     def hash_has_limit(self, token):
         """
@@ -70,7 +94,7 @@ class TemporaryToken:
             return False
         else:
             return True
-   
+
     def hash_get_limit(self, token):
         """
             This function allow to check if token is valid either 
